d3ef8032448f4e3c8d4cbcb86a296a2441d772c8
Leonardo Merlo Primieri <leonardo.mprimieri@gmail.com>
2026-01-01 22:13:20 -0300

    feat: create route to return pix qr code img

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
@@ -33,6 +33,10 @@ def create_pix_payment():
 
 
     return jsonify({"message": "Payment successfully created.", "payment": new_payment.to_dict()})
+
+@app.route('/payments/pix/qr_code/<file_name>', methods=['GET'])
+def get_image_qr_code(file_name):
+    return send_file(f'static/img/{file_name}.png', mimetype='image/png')
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
